031bc9b107d5fd3a2136946ddb4051e875579c1b
LukecZupi <luka.zupan070@gmail.com>
2026-02-08 10:29:09 +0100

    create main.js and TODO comments

---
+++ main.js
@@ -0,0 +1,3 @@
+//DOM event listener
+//gets a random classroom from sola.py, loads the image and displays it on the HTML website
+//checks if the classroom clicked is the right one and acts accordingly
