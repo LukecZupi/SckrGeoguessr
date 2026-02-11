bd4bb1de021cc94d36aadd18821bcae8acfda399
LukecZupi <luka.zupan070@gmail.com>
2026-02-11 07:26:09 +0100

    Add event listeners for classroom elements

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+console.log(document.querySelectorAll(".classroom").length);
+    document.addEventListener("DOMContentLoaded", () => {
+        document.querySelectorAll(".classroom").forEach(el => {
+        el.addEventListener("click", () => {
+            console.log("Clicked classroom:", el.getAttribute("class-data"));
+        });
+        });
+    });
