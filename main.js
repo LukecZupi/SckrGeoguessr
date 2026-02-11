0b567f4951878c33a3a8a037862faaab74ce948e
LukecZupi <luka.zupan070@gmail.com>
2026-02-11 07:58:48 +0100

    js class-data in list

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const classrooms = document.querySelectorAll(".classroom");
-    console.log(classrooms);
-    document.addEventListener("DOMContentLoaded", () => {
-        document.querySelectorAll(".classroom").forEach(el => {
-        console.log(el.getAttribute("class-data"))
-        el.addEventListener("click", () => {
-            console.log("Clicked classroom:", el.getAttribute("class-data"));
-        });
-        });
+document.addEventListener("DOMContentLoaded", () => {
+    const classrooms = [];
+    document.querySelectorAll(".classroom").forEach(el => {
+        const classData = el.getAttribute("class-data");
+        classrooms.push(classData);
     });
+    console.log(classrooms);
+    document.querySelectorAll(".classroom").forEach(el => {
+    el.addEventListener("click", () => {
+        console.log("Clicked classroom:", el.getAttribute("class-data"));
+    });
+    });
+});
