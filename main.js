3ad4d09d5b1efcfa6d1c85ae770231a42049675e
LukecZupi <luka.zupan070@gmail.com>
2026-02-11 07:45:48 +0100

    commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-console.log(document.querySelectorAll(".classroom").length);
+const classrooms = document.querySelectorAll(".classroom");
+    console.log(classrooms);
     document.addEventListener("DOMContentLoaded", () => {
         document.querySelectorAll(".classroom").forEach(el => {
+        console.log(el.getAttribute("class-data"))
         el.addEventListener("click", () => {
             console.log("Clicked classroom:", el.getAttribute("class-data"));
         });
